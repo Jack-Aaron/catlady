da3a87e329f54e9ca20be7097661894255260c7d
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-25 13:11:04 -0500

    working on hasing password for user

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+(bcrypt = require("bcrypt")), (SALT_WORK_FACTOR = 10);
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -11,6 +13,44 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model("User", UserSchema);
 
 const User = mongoose.model("User", UserSchema);
 
