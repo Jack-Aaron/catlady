c65e48aa7cea6c4b5d8633968c4fe4a4a8484609
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-25 16:01:40 -0500

    Password is hasing before going in to database

--- seeds/seedDBcopy.js
+++ seeds/seedDBcopy.js
@@ -10,44 +10,13 @@ mongoose.connect(connStr, (err) => {
 });
 
 // create a user a new user
-const testUser = {
+const testUser = new db.User({
   username: "jmar777",
-  password: "Password123",
-};
+  password: "Password",
+});
 
-// save user to database
-// db.User.remove({})
-//   .then(() =>
-db.User.collection
-  .insertOne(testUser)
-  .then((data) => {
-    console.log(data.result.n + " records inserted!");
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
-
-// fetch user and test password verification
-db.User.collection.findOne({ username: "jmar777" }, (err, user) => {
+testUser.save((err) => {
   if (err) {
     throw err;
   }
-
-  // test a matching password
-  user.comparePassword("Password123", (err, isMatch) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Password123:", isMatch); // -&gt; Password123: true
-  });
-
-  // test a failing password
-  user.comparePassword("123Password", (err, isMatch) => {
-    if (err) {
-      throw err;
-    }
-    console.log("123Password:", isMatch); // -&gt; 123Password: false
-  });
 });
