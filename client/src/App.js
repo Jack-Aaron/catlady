8684e15bc44b1253f87a3609b89ef2e1ba2933a2
Jonathan Alpart <panjialang@gmail.com>
2020-06-26 16:15:32 -0500

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 // import { NavLink as RRNavLink, Router } from 'react-router-dom';
 import Navigation from './components/Navbar';
 import Wrapper from './components/Wrapper';
+<<<<<<< HEAD
 import SignUp from "./pages/SignUp/SignUp";
 import Dashboard from './pages/Dashboard';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+=======
+import Footer from './components/Footer';
+// import { NavLink as RRNavLink, Router } from 'react-router-dom';
+>>>>>>> 9d255c8429974533c3611f8600dbef336a125f08
 import './App.css';
 import { FinalCalculation } from './components/FinalCalculation';
 
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,11 @@ import "./style.css";
 
 function Footer() {
     return (
+<<<<<<< HEAD
         <footer className="footer aladin">
+=======
+        <footer className="footer aladin" style={{ color: '#FFCDB2' }}>
+>>>>>>> 9d255c8429974533c3611f8600dbef336a125f08
             catlady
         </footer>
     );
