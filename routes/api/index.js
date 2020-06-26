1f74aa312b69d98b6abe9a140010984c798d0796
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-26 11:53:10 -0500

    add additional routes to api index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,7 @@ const userRoutes = require("./user");
 
 // Book routes
 router.use("/user", userRoutes);
+router.use("/pet", userRoutes);
+router.use("/petfood", userRoutes);
 
 module.exports = router;
