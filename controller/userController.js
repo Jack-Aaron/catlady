2aee9df0bff704ec0ca933730f641f55679d2b17
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-26 10:42:21 -0500

    controler set to handel each model, created find route in user, test endpoint, success

--- controller/userController.js
+++ controller/userController.js
@@ -1,30 +1,33 @@
-const db = require("../models");
-
 // Defining methods for the booksController
 module.exports = {
-  findAll: function (req, res) {
-    db.User.find(req.query)
+  findAll: function (req, res, model) {
+    model
+      .find(req.query)
       .sort({ date: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   findById: function (req, res) {
-    db.User.findById(req.params.id)
+    model
+      .findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   create: function (req, res) {
-    db.User.create(req.body)
+    model
+      .create(req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   update: function (req, res) {
-    db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
+    model
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
   remove: function (req, res) {
-    db.User.findById({ _id: req.params.id })
+    model
+      .findById({ _id: req.params.id })
       .then((dbModel) => dbModel.remove())
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
