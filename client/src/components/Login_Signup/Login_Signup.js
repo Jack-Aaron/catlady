72dcccc053415c27907f7aa13957d1491f6bcfec
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-29 09:28:37 -0500

    turn sign up form into component that can be shared with sign in page

--- client/src/components/Login_Signup/Login_Signup.js
+++ client/src/components/Login_Signup/Login_Signup.js
@@ -5,12 +5,12 @@ function Login_Signup(props) {
         <div className="row justify-content-center mt-5">
             <div className="col-md-6 col-md-offset-3">
                 <div className="card p-4" id="card" style={{ borderRadius: "2em", boxShadow: "0px 0px 4px 4px #ccc" }}>
-                    <h2>Sign Up Form</h2>
+                    <h2>{props.formname}</h2>
                     <form className="login">
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Email address</label>
                             <input
-                                onChange={handleChange}
+                                onChange={props.handleChange}
                                 name="username"
                                 type="email"
                                 className="form-control"
@@ -19,20 +19,20 @@ function Login_Signup(props) {
                         <div className="form-group">
                             <label htmlFor="exampleInputPassword1">Password</label>
                             <input
-                                onChange={handleChange}
+                                onChange={props.handleChange}
                                 name="password"
                                 type="password"
                                 className="form-control"
                                 placeholder="Password" />
                         </div>
                         <button
-                            disabled={!(formObject.username && formObject.password)}
-                            onClick={handleFormSubmit}
+                            disabled={!(props.formUser && props.formPass)}
+                            onClick={props.handleFormSubmit}
                             type="submit"
-                            className="btn btn-default">Sign Up</button>
+                            className="btn btn-default">{props.buttonText}</button>
                     </form>
                     <br />
-                    <p>Or Log in <a href="./signup.html">here</a></p>
+                    {props.children}
                 </div>
             </div>
         </div>
--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import API from "../../utils/API";
+import Login_Signup from "../../components/Login_Signup/Login_Signup";
 import './SignUp.css'
 
 function SignUp() {
@@ -7,23 +8,32 @@ function SignUp() {
 
     function handleChange(event) {
         const { name, value } = event.target;
-        setFormObject({...formObject, [name]: value})
-      };
+        setFormObject({ ...formObject, [name]: value })
+    };
 
-      function handleFormSubmit(event) {
+    function handleFormSubmit(event) {
         event.preventDefault();
-        if (formObject.username && formObject.password) { 
-          API.saveUser({
-            username: formObject.username,
-            password: formObject.password
-          }).then(setFormObject({}))
-            .catch(err => console.log(err));
+        if (formObject.username && formObject.password) {
+            API.saveUser({
+                username: formObject.username,
+                password: formObject.password
+            }).then(setFormObject({}))
+                .catch(err => console.log(err));
         }
-      };
+    };
 
     return (
         <div className="container">
-            
+            <Login_Signup
+                formname={"Sign Up Form"}
+                handleChange={handleChange}
+                handleFormSubmit={handleFormSubmit}
+                buttonText={"Sign Up"}
+                formUser={formObject.username}
+                formPass={formObject.password}
+            >
+                <p>Or Log in <a href="./signup.html">here</a></p>
+            </Login_Signup>
         </div>
     )
 }
