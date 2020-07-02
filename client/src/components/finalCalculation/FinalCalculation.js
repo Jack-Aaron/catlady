436e9bb9c49710b311df65ebb751b1f278441505
Emily Gaska <emilygaska@gmail.com>
2020-07-02 11:57:34 -0500

    Fixed error that broke cat/dog if statement in FinalCalc

--- client/src/components/finalCalculation/FinalCalculation.js
+++ client/src/components/finalCalculation/FinalCalculation.js
@@ -1,41 +1,63 @@
-import React from "react";
-export const FinalCalculation = ({petState, form}) => {
+import React, { useEffect } from "react";
+export const FinalCalculation = ({ petState, form }) => {
   let name = petState.petName;
-  let weight = petState.currentWeight;
-  let petType = petState.petType;
   let inputFood = form.name;
-  let caloriesPerPackage = form.caloriesPerPackage;
-  let ozPerPackage = form.ozPerPackage;
   let mealNumber = 3;
+  let petType = petState.petType;
   let totalHighEndAmount = 0;
   let totalLowEndAmount = 0;
   let lowEndCalories = 0;
   let highEndCalories = 0;
+  let weight = petState.currentWeight;
+  let caloriesPerPackage = form.caloriesPerPackage;
+  let ozPerPackage = form.ozPerPackage;
 
   let caloriesPerOz = caloriesPerPackage / ozPerPackage;
 
-
-  if (petType === "cat") {
+  if (petType === "Cat") {
     lowEndCalories = weight * 20;
-    totalLowEndAmount = lowEndCalories/caloriesPerOz;
+    console.log("Weight: " + weight);
     highEndCalories = weight * 35;
-    totalHighEndAmount = highEndCalories/caloriesPerOz;
+    totalLowEndAmount = lowEndCalories / caloriesPerOz;
+    totalHighEndAmount = highEndCalories / caloriesPerOz;
+    console.log(
+      "CaloriesPerPackage: " +
+        caloriesPerPackage +
+        " ozPerPackage: " +
+        caloriesPerOz +
+        "Lowend: " +
+        lowEndCalories +
+        "Calories Per oz: " +
+        caloriesPerOz
+    );
+    console.log("TotalLowEndAmount: " + totalLowEndAmount);
   } else {
-    lowEndCalories = (weight * 25);
-    totalLowEndAmount = lowEndCalories/caloriesPerOz;
+    lowEndCalories = weight * 25;
+    totalLowEndAmount = lowEndCalories / caloriesPerOz;
     highEndCalories = weight * 30;
-    totalHighEndAmount = highEndCalories/caloriesPerOz;
-
+    totalHighEndAmount = highEndCalories / caloriesPerOz;
+    console.log(
+      "Lowend: " + lowEndCalories + "Calories Per oz: " + caloriesPerOz
+    );
   }
 
   return (
     <div>
       <h1>{name}</h1>
       <p>
-  Your {petType} will need between {lowEndCalories} and {highEndCalories} calories per day.
+        Your {petType} will need between {lowEndCalories} and {highEndCalories}{" "}
+        calories per day.
       </p>
-  <p>Using {inputFood} they will need between {parseFloat(totalLowEndAmount).toFixed(2)} and {parseFloat(totalHighEndAmount).toFixed(2)} oz per day to maintain their current weight.</p>
-  <p>That is between {parseFloat(totalLowEndAmount/mealNumber).toFixed(2)} and {parseFloat(totalHighEndAmount/mealNumber).toFixed(2)} oz per meal.</p>
+      <p>
+        Using {inputFood} they will need between{" "}
+        {parseFloat(totalLowEndAmount).toFixed(2)} and{" "}
+        {parseFloat(totalHighEndAmount).toFixed(2)} oz per day to maintain their
+        current weight.
+      </p>
+      <p>
+        That is between {parseFloat(totalLowEndAmount / mealNumber).toFixed(2)}
+        and {parseFloat(totalHighEndAmount / mealNumber)} oz per meal.
+      </p>
     </div>
   );
 };
