fb6cb7c527a650e20175599095920d3a19287085
Jonathan Alpart <panjialang@gmail.com>
2020-07-05 17:42:39 -0500

    converting div soup to react-bootstrap

--- client/src/components/LoginSignupForm/LoginSignupForm.js
+++ client/src/components/LoginSignupForm/LoginSignupForm.js
@@ -1,44 +1,61 @@
-import React from "react"
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from '../Container';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 
 function LoginSignupForm(props) {
     return (
-        <div className="row justify-content-center pt-5">
-            <div className="col-md-6 col-md-offset-3">
-                <div className="card p-4" id="card" style={{ borderRadius: "2em", boxShadow: "0px 0px 4px 4px #ccc" }}>
-                    <h2>{props.formname}</h2>
-                    <form className="login">
-                        <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input
-                                onChange={props.handleChange}
-                                name="username"
-                                type="email"
-                                className="form-control"
-                                placeholder="Email (required)"
-                                value={props.uValue} />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputPassword1">Password</label>
-                            <input
-                                onChange={props.handleChange}
-                                name="password"
-                                type="password"
-                                className="form-control"
-                                placeholder="Password"
-                                value={props.pValue} />
-                        </div>
-                        <button
-                            disabled={!(props.formUser && props.formPass)}
-                            onClick={props.handleFormSubmit}
-                            type="submit"
-                            className="btn btn-primary">{props.buttonText}
-                        </button>
-                    </form>
-                    <br />
-                    {props.children}
-                </div>
-            </div>
-        </div>
+        <Container className='pb-3'>
+            <Row className="row justify-content-center pt-3 pb-3">
+                <Col className="col-md-6 col-md-offset-3">
+                    <Card className="card px-4" id="card" style={{
+                        borderRadius: "2em",
+                        boxShadow: "0px 0px 4px 4px #ccc",
+                    }}>
+                        <Card.Body>
+                            <Card.Title style={{ textAlign: 'center',
+                        height:0 }}>{props.formname}</Card.Title>
+                            <Form className="login">
+                                <Form.Group className="form-group">
+                                    <Form.Label htmlFor="exampleInputEmail1">Email address</Form.Label>
+                                    <Form.Control
+                                        onChange={props.handleChange}
+                                        name="username"
+                                        type="email"
+                                        className="form-control"
+                                        placeholder="Email (required)"
+                                        value={props.uValue} />
+                                </Form.Group>
+                                <Form.Group className="form-group">
+                                    <Form.Label htmlFor="exampleInputPassword1">Password</Form.Label>
+                                    <Form.Control
+                                        onChange={props.handleChange}
+                                        name="password"
+                                        type="password"
+                                        className="form-control"
+                                        placeholder="Password"
+                                        value={props.pValue} />
+                                </Form.Group>
+                                <Button
+                                    disabled={!(props.formUser && props.formPass)}
+                                    onClick={props.handleFormSubmit}
+                                    type="submit"
+                                    className="btn btn-primary">{props.buttonText}
+                                </Button>
+                                <Form.Group className='pt-2'>
+                                {props.children}
+                                </Form.Group>
+                            </Form>
+                          
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
