7193f9992e0226cb5d8bda013e18194e377155d4
Emily Gaska <emilygaska@gmail.com>
2020-07-07 18:29:54 -0500

    For merge

---
+++ client/build/precache-manifest.f3c2bf5fad0429f78d7b6cf66c91a2b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bb767067991db5287a2b96264697d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8886c98634941d7f7887",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "65798f39b7b95cb58c42",
+    "url": "/static/css/main.b2019c1a.chunk.css"
+  },
+  {
+    "revision": "8886c98634941d7f7887",
+    "url": "/static/js/2.73adc7bf.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.73adc7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65798f39b7b95cb58c42",
+    "url": "/static/js/main.cde54a63.chunk.js"
+  },
+  {
+    "revision": "8b3a4addca20320984b6",
+    "url": "/static/js/runtime-main.fa0be5f9.js"
+  },
+  {
+    "revision": "8e5986a7ff3370c13afbc5a587dd9d11",
+    "url": "/static/media/cat.8e5986a7.png"
+  },
+  {
+    "revision": "8dcf68edae1a6c56cebf9ac5df355518",
+    "url": "/static/media/dog.8dcf68ed.png"
+  }
+]);
