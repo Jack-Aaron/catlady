b34a0cee35674525c2e7741b2aa9e2e074963964
Jonathan Alpart <panjialang@gmail.com>
2020-07-07 13:36:25 -0500

    new build to heroku

---
+++ client/build/precache-manifest.deab767db8c0db2deea607809dd17d7b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8767a74d96d7471b7bcdb58d5c7709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3f243bba46d6352d304",
+    "url": "/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "0150c6e1e348be992aa3",
+    "url": "/static/css/main.8c55cecd.chunk.css"
+  },
+  {
+    "revision": "c3f243bba46d6352d304",
+    "url": "/static/js/2.a136f1cf.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.a136f1cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0150c6e1e348be992aa3",
+    "url": "/static/js/main.9aaab7df.chunk.js"
+  },
+  {
+    "revision": "8b3a4addca20320984b6",
+    "url": "/static/js/runtime-main.fa0be5f9.js"
+  },
+  {
+    "revision": "8e5986a7ff3370c13afbc5a587dd9d11",
+    "url": "/static/media/cat.8e5986a7.png"
+  },
+  {
+    "revision": "8dcf68edae1a6c56cebf9ac5df355518",
+    "url": "/static/media/dog.8dcf68ed.png"
+  }
+]);
